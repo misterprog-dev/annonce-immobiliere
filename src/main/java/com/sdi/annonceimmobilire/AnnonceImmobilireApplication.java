58f205e96c28faee39e9c2f8fad12ab0a8ec3312
DIAKITE SOUMAILA <diakite.soumaila@externe.gfit.fr>
2021-11-30 22:21:49 +0100

    first commit

---
+++ src/main/java/com/sdi/annonceimmobilire/AnnonceImmobilireApplication.java
@@ -0,0 +1,13 @@
+package com.sdi.annonceimmobilire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnonceImmobilireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnonceImmobilireApplication.class, args);
+	}
+
+}
