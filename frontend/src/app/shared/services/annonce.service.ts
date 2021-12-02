b973432cd14c5eb06db5c2c4050d79e17d71c4c1
DIAKITE SOUMAILA <diakite.soumaila@externe.gfit.fr>
2021-12-03 00:27:20 +0100

    AIM-4 : Ajout des service de CRUD

--- frontend/src/app/shared/services/annonce.service.ts
+++ frontend/src/app/shared/services/annonce.service.ts
@@ -22,4 +22,50 @@ export class AnnonceService {
 				map(annonces => annonces.map(annonce => new Annonce().deserialize(annonce)))
 		);
 	}
+
+	/**
+	 * Get one ad.
+	 * @param id id of ad
+	 */
+	readAd(id: number): Observable<Annonce> {
+		return this.http.get<Annonce>(`${this.url}/${id}`).pipe(
+				map(annonce => new Annonce().deserialize(annonce)));
+	}
+
+	/**
+	 * Create a ad.
+	 * @param annonce the ad.
+	 * @param uploadedFile image of ad.
+	 */
+	createAd(annonce: Annonce, uploadedFile: File): Observable<void> {
+		const formData = new FormData();
+		formData.append('image', uploadedFile);
+
+		const blobAnnonce= new Blob([JSON.stringify(annonce.serialize())], {
+			type: 'application/json',
+		});
+		formData.append('annonce', blobAnnonce);
+
+		return this.http.post<void>(this.url, formData);
+	}
+
+	updateAd(id: number, annonce: Annonce, uploadedFile: File): Observable<void> {
+		const formData = new FormData();
+		formData.append('image', uploadedFile);
+
+		const blobAnnonce= new Blob([JSON.stringify(annonce.serialize())], {
+			type: 'application/json',
+		});
+		formData.append('annonce', blobAnnonce);
+
+		return this.http.put<void>(`${this.url}/${id}`, formData);
+	}
+
+	/**
+	 * Delete the ad.
+	 * @param id id of Ad.
+	 */
+	deleteAd(id: number): Observable<void> {
+		return this.http.delete<void>(`${this.url}/${id}`,{});
+	}
 }
